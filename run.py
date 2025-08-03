b18b546443daeb4106ecdf2f9a8443191e802806
R Sri Sudarshan <srisudarshanrg@gmail.com>
2025-08-03 10:29:58 +0530

    adding requirements

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
-print("for initial commit")
+import ollama
+
+client = ollama.Client()
+
+model = "llama3.2"
+prompt = "what is python"
+
+response = client.generate(model=model, prompt=prompt)
+
+print(response.response)
